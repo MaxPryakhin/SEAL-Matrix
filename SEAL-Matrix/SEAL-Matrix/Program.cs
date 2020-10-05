283b97df78c2c7c1557f488af1022f754ea7d705
Max Pryakhin <Maxim.K.Pryakhin@gmail.com>
2020-10-05 11:43:11 +0300

    start project

---
+++ SEAL-Matrix/SEAL-Matrix/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SEAL_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
