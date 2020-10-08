53ed37e0158f8176cc40b0d0d3effa2b42b36a85
Max Pryakhin <KlaronFirefly@gmail.com>
2020-10-08 12:30:46 +0300

    Menu

--- SEAL-Matrix/SEAL-Matrix/Program.cs
+++ SEAL-Matrix/SEAL-Matrix/Program.cs
@@ -1,6 +1,9 @@
 ﻿using SEAL_Matrix.Core.Matrix;
 using System;
 using Microsoft.Research.SEAL;
+using System.Linq;
+using System.Diagnostics;
+using System.Globalization;
 
 namespace SEAL_Matrix
 {
@@ -8,42 +11,97 @@ namespace SEAL_Matrix
     {
         static void Main(string[] args)
         {
-            var matrix = new Matrix
-            {
-                Elements = new double[] { 1.0, 0.5, 2.0, 5.0 },
-                RowSize = 2
-            };
+            int command = DisplayMenu();
 
             var matrixStrategy = new MatrixStrategy();
             var matrixContext = new MatrixContext(matrixStrategy);
-            
-            Console.WriteLine("initial matrix");
-            //TODO: change init
-            matrixContext.Matrix = matrix;
-
-            //Console.WriteLine("multiply by number");
-            //matrixContext.MultiplyMatrixByNumber(5);
-
-            Console.WriteLine("add matrix");
-            matrixContext.AddMatrix(matrix);
-
             var matrixHomomorphicStrategy = new MatrixHomomorphicStrategy();
             var matrixHomomorphicContext = new MatrixContext(matrixHomomorphicStrategy);
 
-            Console.WriteLine("initial homomorphic matrix");
-            matrix = new Matrix
+            while (command != 5)
             {
-                Elements = new double[] { 1.0, 0.5, 2.0, 5.0 },
-                RowSize = 2
+                var firstMatrix = InitMatrix();
+                var firstCopyMatrix = new Matrix()
+                {
+                    Elements = (double[])firstMatrix.Elements.Clone(),
+                    RowSize = firstMatrix.RowSize
+                };
+
+                matrixContext.Matrix = firstMatrix;
+                matrixHomomorphicContext.Matrix = firstCopyMatrix;
+                switch (command)
+                {
+                    case 1:
+                        var number = InitNumber();
+                        matrixContext.MultiplyMatrixByNumber(number);
+                        matrixHomomorphicContext.MultiplyMatrixByNumber(number);
+                        break;
+                    case 2:
+                        var secondAddMatrix = InitMatrix();
+                        matrixContext.AddMatrix(secondAddMatrix);
+                        matrixHomomorphicContext.AddMatrix(secondAddMatrix);
+                        break;
+                }
+
+                if (command != 5)
+                {
+                    CompareMatrix(firstMatrix, firstCopyMatrix);
+                }
+
+                command = DisplayMenu();
+            }
+        }
+
+        static public int DisplayMenu()
+        {
+            Console.WriteLine("Matrix operation");
+            Console.WriteLine();
+            Console.WriteLine("1. Multiply matrix by number");
+            Console.WriteLine("2. Add matrix to matrix");
+            //Console.WriteLine("3. ");
+            //Console.WriteLine("4. ");
+            Console.WriteLine("5. Exit");
+            var result = Console.ReadLine();
+            return Convert.ToInt32(result);
+        }
+
+        static public Matrix InitMatrix()
+        {
+            Console.WriteLine("Row size (2): ");
+            var rowSize = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Matrix (1,0 2,0 0,5 5,0 ...): ");
+            var result = Console.ReadLine().ToString(CultureInfo.InvariantCulture);
+            var elements = result.Split(" ").Select(s => Convert.ToDouble(s)).ToArray();
+            return new Matrix
+            {
+                Elements = elements,
+                RowSize = rowSize
             };
-            //TODO: change init
-            matrixHomomorphicContext.Matrix = matrix;
+        }
 
-            //Console.WriteLine("multiply by number");
-            //matrixHomomorphicContext.MultiplyMatrixByNumber(5);
+        static public double InitNumber()
+        {
+            Console.WriteLine("Number (5,0): ");
+            return Convert.ToDouble(Console.ReadLine());
+        }
 
-            Console.WriteLine("add matrix");
-            matrixHomomorphicContext.AddMatrix(matrix);
+        static public void CompareMatrix(Matrix a, Matrix b)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Compare matrix: ");
+            Console.WriteLine();
+            var aElements = a.Elements;
+            var bElements = b.Elements;
+            for (int i = 0; i < aElements.Length; i++)
+            {
+                Console.WriteLine($"element at {i}");
+                Console.WriteLine($"{aElements[i]}");
+                Console.WriteLine($"{bElements[i]}");
+                Console.WriteLine();
+            }
+            Console.WriteLine($"/{new string('-', 10)}/");
+            Console.WriteLine();
         }
     }
 }
